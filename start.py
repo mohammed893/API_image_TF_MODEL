9690e7fd0b7f793d19a4b77e4fa3b93ec754ab9b
Mohammed_sherif <72109951+mohammed893@users.noreply.github.com>
2024-01-16 12:30:13 +0200

    Update start.py

--- start.py
+++ start.py
@@ -4,6 +4,8 @@ import tensorflow_hub as hub
 import pandas as pd
 import os
 import numpy as np
+import matplotlib as mpl
+from IPython.display import Image, display
 
 ##----------------------------will be pre prepared by AI Team------------------------------------###
 
@@ -83,11 +85,92 @@ load a saved model from a path
   model = tf.keras.models.load_model(model_path, custom_objects={"KerasLayer" : hub.KerasLayer})
   return model
 #->Given the image path you will Predict the label
-def predict (path):
-    test_filenames = [path]
-    test_data = create_data_batches(test_filenames, test_data=True)
-    test_predictions = model.predict(test_data, verbose = 0)
-    return f"label:{unique[np.argmax(test_predictions[0])]}"
+
+def get_img_array(img_path, size = (224 , 224)):
+    # `img` is a PIL image of size 299x299
+    img = tf.keras.utils.load_img(img_path, target_size=size)
+    # `array` is a float32 Numpy array of shape (299, 299, 3)
+    array = tf.keras.utils.img_to_array(img)
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, 299, 299, 3)
+    array = np.expand_dims(array, axis=0)
+    return array
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        model.inputs, [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+def decode_predictions(preds):
+  classes = ['Glioma' , 'meningioma' , 'No Tumor' , 'Pituitary']
+  prediction = classes[np.argmax(preds)]
+  return prediction
+
+def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.4 , view = False):
+    # Load the original image
+    img = tf.keras.utils.load_img(img_path)
+    img = tf.keras.utils.img_to_array(img)
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    jet = mpl.colormaps["jet"]
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = tf.keras.utils.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = tf.keras.utils.img_to_array(jet_heatmap)
+
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = tf.keras.utils.array_to_img(superimposed_img)
+
+    # Save the superimposed image
+    superimposed_img.save(cam_path)
+
+    
+def make_prediction (img_path , model, last_conv_layer_name = "Top_Conv_Layer" ,
+                      campath = "static\cam.jpg" ,
+                        view = False):
+  img = get_img_array(img_path = img_path)
+  img_array = get_img_array(img_path, size=(224 , 224))
+  preds = model.predict(img_array)
+  heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+  save_and_display_gradcam(img_path, heatmap , cam_path=campath , view = view)
+  return [campath , decode_predictions(preds)]
+
 
 ##----------------------------will be pre prepared by AI Team------------------------------------###
 
@@ -102,7 +185,7 @@ app = Flask(__name__)
 
 
 #Loading The DeepLearning Model 
-model = load_model("API_image_TF_MODEL-main/20231116-05051700111111-1000.h5")
+model = load_model("my_model.h5")
 
 #path for the testing photos
 test_path = "API_image_TF_MODEL-main\static"
@@ -117,13 +200,13 @@ def main():
 #The submit Root (The route used to predict)
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
+    
 	if request.method == 'POST':
 		img = request.files['my_image']
-		img_path = "API_image_TF_MODEL-main/static/" + img.filename	
-		img.save(img_path) 
-		prediction = predict(img_path)
-	return prediction
-
+		img_path = "static/" + img.filename	
+		img.save("static/" + img.filename	) 
+		prediction_1 = make_prediction(img_path, model = model)[1]
+	return render_template("index.html", prediction = prediction_1, img_path = "static\cam.jpg" , img_pre = img_path)
 
 if __name__ =='__main__':
 	#app.debug = True
